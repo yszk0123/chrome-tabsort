a59ccf0f57d0af706b19225062f3016c96d7e437
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-07-22 08:07:13 +0900

    test: Fix tests

--- test/Divider.js
+++ test/Divider.js
@@ -23,9 +23,9 @@ test('Divider#divide', (tDivider) => {
     tNoRule.test('too much tabs per window', (t) => {
       const items = [
         { id: 1, url: 'http://a.com/1' },
-        { id: 2, url: 'http://a.com/2' },
-        { id: 3, url: 'http://a.com/3' },
-        { id: 4, url: 'http://b.co.jp/4' },
+        { id: 2, url: 'http://b.com/2' },
+        { id: 3, url: 'http://c.com/3' },
+        { id: 4, url: 'http://d.com/4' },
       ];
       const divider = new Divider([]);
       t.deepEqual(divider.divide(items, 2).map(getIds), [[1, 2], [3, 4]]);
@@ -36,7 +36,7 @@ test('Divider#divide', (tDivider) => {
   });
 
   tDivider.test('with rules', (tRule) => {
-    tRule.test('tabsPerWindow', (t) => {
+    tRule.test('including all tabs in one window', (t) => {
       const items = [
         { id: 1, url: 'http://a.com/1' },
         { id: 2, url: 'http://a.com/2' },
@@ -51,24 +51,21 @@ test('Divider#divide', (tDivider) => {
       t.end();
     });
 
-    tRule.test('should ', (t) => {
+    tRule.test('too much tabs per window', (t) => {
       const items = [
-        { id: 1, url: 'http://a.com/a' },
-        { id: 2, url: 'http://a.com/nextgroup/a' },
-        { id: 2, url: 'http://a.com/nextgroup/b' },
-        { id: 3, url: 'http://b.co.jp/c' },
-        { id: 4, url: 'http://b.co.jp/doc/isolate' },
-        { id: 5, url: 'http://b.co.jp/d' },
-        { id: 6, url: 'http://b.co.jp/e' },
-        { id: 7, url: 'http://b.co.jp/f' },
-        { id: 10, url: 'http://c.ne.jp/g' },
+        { id: 1, url: 'http://a.com/1' },
+        { id: 2, url: 'http://b.co.jp/c' },
+        { id: 3, url: 'http://a.com/2' },
+        { id: 4, url: 'http://b.co.jp/d' },
+        { id: 5, url: 'http://b.co.jp/doc/isolate' },
       ];
       const rules = [
         { regexp: '\\.com\\b' },
         { regexp: '\\bdoc|\\bapi', isolate: true },
       ];
       const divider = new Divider(rules);
-      t.deepEqual(divider.divide(items, 3), {});
+      t.deepEqual(divider.divide(items, 3).map(getIds), [[5], [1, 3], [2, 4]]);
+      t.end();
     });
 
     tRule.end();

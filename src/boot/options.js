fb577174044b0cdfcf8c24975e4b5f69701db329
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-11-29 18:23:52 +0900

    refactor(boot): Move startup files to boot

--- src/boot/options.js
+++ src/boot/options.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
 import Options from '../containers/Options'
-import store from './store'
+import store from '../store'
 
 render((
   <Provider store={store}>

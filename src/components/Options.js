36acaf2179c2bebc1b56dd14a5b88e00226bc956
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-11-30 10:23:23 +0900

    feat(options, components): Clean up

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,117 +1,22 @@
-'use strict'
 import React from 'react'
-import FluxComponent from 'Flummox/component'
-import { pull } from '../utils/utils'
-import appState from '../utils/appState'
-import OptionsConfig from '../constants/Options'
+import { connect } from 'react-redux'
 
-export default class Options {
-  render() {
-    return (
-      <FluxComponent
-        connectToStores={{
-          options: flux.getStore('optionsStore')
-        }}
-        render={({groupPropsList}) =>
-          <div>
-            {groupPropsList.map((groupProps) =>
-              <Group groupProps={groupProps}
-                     optionsActions={optionsActions} />
-            )}
-          </div>
-        }
-      />
-    )
-  }
-}
+import Group from '../components/Group'
 
-class Group extends React.Component {
+export class Options {
   render() {
-    const { name, tabsPerWindow } = this.props.groupProps
+    const { groupPropsList } = this.props
 
     return (
       <div>
-        <div>名前: {name}</div>
-        <div>ウィンドウ毎のタブ数: {tabsPerWindow}</div>
+        {groupPropsList.map((groupProps) => <Group {...groupProps} />)}
       </div>
     )
   }
 }
 
-export default class OptionsActions {
+Options.propTypes = {
+  groupPropsList: PropTypes.array.isRequired,
 }
 
-export default class Options extends React.Component {
-  // save() {
-  //   appState.update({
-  //     store: {
-  //       tabsPerWindow: { $set: this.props.tabsPerWindow },
-  //       rules: { $set: this.props.rules },
-  //     },
-  //     saved: { $set: true },
-  //   })
-  //   this.props.viewState.save()
-  //   // this.context.cursors.groupPropsList.set(this.props.groupPropsList)
-  // }
-  //
-  // removeAt(rule) {
-  //   appState.update({
-  //     store: {
-  //       rules: { $apply: (rules) => pull(this.props.rules, rule) }
-  //     }
-  //   })
-  // }
-  //
-  // restore() {
-  //   this.props.update({
-  //     tabsPerWindow: { $set: this.props.tabsPerWindow },
-  //     rules: { $set: this.props.rules },
-  //   })
-  // }
-  //
-  // addRules() {
-  //   appState.update({
-  //     store: {
-  //       rules: { $push: assign({}, OptionsConfig.defaultRule) }
-  //     }
-  //   })
-  // }
-  //
-  // upRule(rule) {
-  //   const rules = this.props.rules
-  //   const i = findIndex(rules, rule)
-  //   if (i >= 1) {
-  //     swapInArray(rules, i, i - 1)
-  //   }
-  //   appState.update({
-  //     $set: { rules }
-  //   })
-  // }
-  //
-  // downRule(rule) {
-  //   const rules = this.props.rules
-  //   const i = findIndex(rules, rule)
-  //   if (i >= 0 && i <= rules.length - 2) {
-  //     swapInArray(rules, i, i + 1)
-  //   }
-  //   appState.update({
-  //     $set: { rules }
-  //   })
-  // }
-
-  render() {
-    return (
-      <div>
-      </div>
-    )
-  }
-}
-
-function findIndex(array, element) {
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] === element) {
-      return i
-    }
-  }
-  return -1
-}
+export default connect((state) => {}, {}, Options)

a2ad8b5fe470088e7f16a5d0d95dbfe8b88b25e0
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-12-15 13:29:55 +0900

    refactor: Update directory structore

--- src/boot/options.js
+++ src/boot/options.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Options from '../containers/Options';
-import store from '../store';
-import * as optionsActions from '../actions/Options';
+import Options from '../background/containers/Options';
+import store from '../common/store';
+import * as optionsActions from '../common/actions/Options';
 
 store.dispatch(optionsActions.load());
 

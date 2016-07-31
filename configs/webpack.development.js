3cdce228ca083eb98151a059be232d95a08522df
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-08-01 02:47:44 +0900

    chore(webpack): fix paths

--- configs/webpack.development.js
+++ configs/webpack.development.js
@@ -4,29 +4,31 @@ const webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: {
-    background: './src/boot/background',
-    options: './src/boot/options'
+    background: './packages/ts-app/background',
+    options: './packages/ts-app/options',
   },
   output: {
     path: path.join(__dirname, '..', 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
-    loaders: [{
-      test: /\.css$/,
-      loaders: ['style', 'css'],
-      include: path.join(__dirname, '..', 'src')
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(__dirname, '..', 'src'),
-      query: {
-        cacheDirectory: true
-      }
-    }]
-  }
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.join(__dirname, '..', 'src')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: path.join(__dirname, '..', 'src'),
+        query: {
+          cacheDirectory: true
+        }
+      },
+    ],
+  },
 };
